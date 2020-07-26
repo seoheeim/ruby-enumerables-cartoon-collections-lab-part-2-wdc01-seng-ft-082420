6566312d8e7c52406dbd7626a274b56f5e24e931
Seohee  Im <seoheeim@vt.edu>
2020-07-26 01:29:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,6 @@ end
 def summon_captain_planet(planeteer_calls)
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
-  planeteer_calls.map.capitalize()
 
   }
 end
